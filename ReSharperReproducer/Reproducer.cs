3cc37feaea540121e7190fce339d89f3190e1afb
Sebastian Krysmanski <noreply@manski.net>
2020-10-01 10:40:07 +0200

    Added reproducer

--- ReSharperReproducer/Reproducer.cs
+++ ReSharperReproducer/Reproducer.cs
@@ -1,14 +1,32 @@
 ﻿//
 // This file contains a reproducer for a ReSharper bug.
 //
+using System;
 
 namespace ReSharperReproducer
 {
-    public class Reproducer
+    public sealed class MyTestClass
     {
-        public void ReproducerMethod()
+        public MyTestClass Append(ConsoleColor? color, string value)
         {
+            return this;
+        }
 
+        public void MethodWithLocalFunction(object operand1)
+        {
+            // ReSharper disable once UnusedLocalFunctionReturnValue
+            MyTestClass MyLocalFunction(object operand) // <-- missing "Local function can be made static"
+            {
+                switch (operand)
+                {
+                    case string uncoloredString:
+                        return new MyTestClass().Append(color: null, uncoloredString);
+                }
+
+                throw new Exception();
+            }
+
+            MyLocalFunction(operand1);
         }
     }
 }
